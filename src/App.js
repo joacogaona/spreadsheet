5395ac2a0f1c1086864e3d60ca3d0d5293a19d58
joaco <jgaona@keiron.cl>
2022-06-24 15:51:30 -0500

    setup con CRA y tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola</h1>
     </div>
   );
 }
 
 export default App;
+
+// RESALTADOR DE CELDA PARA SABER DONDE ESTOY
+
+// CELDA
+// Puedo escribir y si pongo un = pasa a ser una formula u operacion a resolver
+// Se pueden hacer sumas y restas entre numeros y entre referencias de celda
+
+//TABLA
+
+//COLUMNAS
+
+//FILAS
+
+//VISOR DE FORMULAS
+
+//BUSCADOR DE CELDAS
