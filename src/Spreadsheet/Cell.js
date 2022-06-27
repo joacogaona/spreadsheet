aa5b7d6dfe77753ca4e7584dc551856758122b96
joaco <jgaona@keiron.cl>
2022-06-26 20:44:34 -0500

    agrego mathjs en vez de eval

--- src/Spreadsheet/Cell.js
+++ src/Spreadsheet/Cell.js
@@ -1,4 +1,5 @@
 import { normStringAndRefCells } from "../utils";
+import { evaluate } from "mathjs";
 
 function Cell({
   handleSelect,
@@ -24,7 +25,7 @@ function Cell({
     try {
       if (cellsContent[cellId]?.[0] === "=") {
         const stringToEval = normStringAndRefCells({ cellsContent, cellId });
-        const result = eval(stringToEval);
+        const result = evaluate(stringToEval);
         setCellsContent((prevState) => {
           const newState = { ...prevState };
           newState[cellId] = result;
@@ -46,7 +47,7 @@ function Cell({
   }
 
   return (
-    <td className={`border-2  h-10 m-0 p-0 min-w-28 `} onClick={handleSelect}>
+    <td className={`border-2  h-10 m-0 p-0 min-w-28 `}>
       <input
         className={`h-full  ${
           isSelected ? "bg-sky-100" : cellsErrors[cellId] ? "bg-red-200" : ""
@@ -58,6 +59,7 @@ function Cell({
         onChange={handleContent}
         onBlur={handleBlur}
         onKeyDown={handleEnter}
+        onClick={handleSelect}
       />
     </td>
   );
